2c95b7befb250b9d042451c47db6a2cb15c5b744
saheelwagh <saheelwagh@gmail.com>
2022-12-03 22:00:58 +0530

    applied some styles to home page

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -8,13 +8,16 @@ import { useRouter } from 'next/dist/client/router'
 export default function Home() {
   const router = useRouter()
   return (
-    <main>
-      <div>Welconme to Knowmyrecords</div>
-      <div>View More</div>
+    <main className='h-screen grid place-content-center'>
+      <div className='text-5xl'>Welconme to Knowmyrecords</div>
+      <div className='text-3xl'>View More</div>
+      <form action="" className='bg-slate-50 flex justify-center'>
       <div
-      className="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-5"
+      className="bg-green-500 hover:bg-green-700 w-32 text-white font-bold py-2 px-4 rounded m-5"
       onClick={() => router.push('/Dashboard') }
       >Authenticate Pending Requests</div>
+      </form>
+      
     </main>
   )
 }
