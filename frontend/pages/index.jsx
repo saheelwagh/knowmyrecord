d79049021547f654e18dbe5f72e020ac41e4dafc
saheelwagh <saheelwagh@gmail.com>
2022-12-03 18:53:03 +0530

    implemented routing on home page

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -2,17 +2,18 @@ import { Main } from 'next/document'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
+import Link from 'next/link'
+import { useRouter } from 'next/dist/client/router'
+ 
 export default function Home() {
+  const router = useRouter()
   return (
     <main>
       <div>Welconme to Knowmyrecords</div>
       <div>View More</div>
       <div
       className="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-5"
-      onClick={() => {
-        
-      }}
+      onClick={() => router.push('/Dashboard') }
       >Authenticate Pending Requests</div>
     </main>
   )
