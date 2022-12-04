811b6296a2bd55658289ef063d64f59b8886aa79
saheelwagh <saheelwagh@gmail.com>
2022-12-04 06:31:24 +0530

    added connect logic

--- frontend/pages/Dashboard.jsx
+++ frontend/pages/Dashboard.jsx
@@ -3,25 +3,36 @@ export default function DashBoard({}) {
     useEffect(()=>console.log(hospitals), [])
 const [hospitals, setHospitals] = useState([
     {
-        date : '05 Nov',
+        date : '05 Nov 2022',
         name: 'Hospital 1'
     },{
-        date : '01 Nov',
+        date : '01 Nov 2022',
         name: 'Hospital 2'
     },
     {
-        date : '03 Dec',
+        date : '03 Dec 2022',
         name: 'Hospital 3'
     }
 ])
 return (
-    <main>
-        <div className="card1">
-            
-            <h6>Time</h6>
-            <h3>Hospital Name</h3>
-            <div className="button">View</div>
-        </div>
+    <main className=" grid place-content-center h-screen">
+        <h1 className="text-3xl">Your Records</h1>
+       <ul>
+            <li>
+                <span className="text-xl text-red-500 pr-5">New Access Request:</span>
+                <button className="text-xl text-blue-500"
+                onClick={()=>{/* fetch pending trans from chain*/ }}
+                >Click To View</button>
+            </li>
+            {hospitals.map(hosp=>
+            <li className="data-card border-2 border-yellow-300 h-48">
+                <div className="text-slate-500 font-bold">{hosp.date}
+                </div>
+                <div className="text-xl">
+                  Accessed by:  {hosp.name}
+                </div>
+                </li>)}
+        </ul>
     </main>
 )
 }
--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -4,19 +4,53 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import { useRouter } from 'next/dist/client/router'
- 
+ import {useState, useEffect, useRef } from 'react'
+ import Web3Modal from "web3modal";
+import { providers, Contract } from "ethers";
+import { WHITELIST_CONTRACT_ADDRESS, abi } from "../constants";
+
+const getProviderOrSigner = async (needSigner = false) => {
+  // Connect to Metamask
+  // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
+  const provider = await web3ModalRef.current.connect();
+  const web3Provider = new providers.Web3Provider(provider);
+
+  // If user is not connected to the Goerli network, let them know and throw an error
+  const { chainId } = await web3Provider.getNetwork();
+  if (chainId !== 5) {
+    window.alert("Change the network to Goerli");
+    throw new Error("Change network to Goerli");
+  }
+
+  if (needSigner) {
+    const signer = web3Provider.getSigner();
+    return signer;
+  }
+  return web3Provider;
+};
+
+function connectWallet(){
+  console.log('attempting metamask connection')
+} 
 export default function Home() {
+  const [walletConnected, setWalletConnected] = useState(false)
+  const [loading, setLoading] = useState(false);
+  const web3ModalRef = useRef();
   const router = useRouter()
   return (
     <main className='h-screen grid place-content-center'>
       <div className='text-5xl'>Welconme to Knowmyrecords</div>
-      <div className='text-3xl'>View More</div>
-      <form action="" className='bg-slate-50 flex justify-center'>
+      
+      <div
+      className="bg-orange-500 hover:bg-orange-700 w-24 text-white font-bold py-2 px-4 rounded m-5"
+      onClick={ connectWallet }
+      >Connect Wallet</div>
+      <div className='bg-slate-50 flex justify-center'>
       <div
       className="bg-green-500 hover:bg-green-700 w-32 text-white font-bold py-2 px-4 rounded m-5"
       onClick={() => router.push('/Dashboard') }
       >Authenticate Pending Requests</div>
-      </form>
+      </div>
       
     </main>
   )
